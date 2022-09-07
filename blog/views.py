09d2a93380c1f339348815193d8b116f9fa1e339
spacehur <spacehur@gmail.com>
2022-09-07 16:47:01 +0500

    MON-9 [add] adding button for new post with authorize

--- blog/views.py
+++ blog/views.py
@@ -17,11 +17,18 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+
+            if request.user.is_authenticated:
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+
+            else:
+                form = PostForm()
+                return render(request, 'blog/post_error.html', {'form': form})
+
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
