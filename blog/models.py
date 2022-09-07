35e73e3ef38a617adf69d038f5b0aa0d07fc4341
Khurshed <59733417+khurshedz@users.noreply.github.com>
2022-09-07 17:32:06 +0500

    Revert "Ticket mon 9"

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 import datetime
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     title = models.CharField(max_length = 200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -17,18 +17,11 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-
-            if request.user.is_authenticated:
-                post = form.save(commit=False)
-                post.author = request.user
-                post.published_date = timezone.now()
-                post.save()
-                return redirect('post_detail', pk=post.pk)
-
-            else:
-                form = PostForm()
-                return render(request, 'blog/post_error.html', {'form': form})
-
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
