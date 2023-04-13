d6d029ba0239f8385db8171565a34b34d34bc830
kzulpanov <kzulpanov@dengabank.ru>
2023-04-14 00:46:53 +0300

    [fix] renew after long year

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6s=cj^5)(b3o@76jykueb1e_(m#!*_r+x*5+(6zfin*gk=-jh*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
@@ -137,5 +137,5 @@ LOGGING = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '~/git/testgit/hura/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
