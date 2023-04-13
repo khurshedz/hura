011ef53ca8049da6020d0f8b2fb609cdf24ae534
kzulpanov <kzulpanov@dengabank.ru>
2023-04-14 01:00:52 +0300

    [fix] fixes codestyle loke PEP8

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
@@ -12,4 +12,4 @@ urlpatterns = {
     path('contacts/', views.contacts, name='contacts'),
     path('unique/', views.unique, name='unique'),
 
-}
+]
